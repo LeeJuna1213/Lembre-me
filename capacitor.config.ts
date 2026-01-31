04a05d609c495c0d1c1b8c25be016c559af80b53
LeeJuna1213 <jainicialefinal@gmail.com>
2026-01-31 01:16:26 -0300

    modificações para apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jully.lembreme',
   appName: 'lembre-me',
   webDir: 'www'
 };
